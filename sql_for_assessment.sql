2497365ef00693ea713fcbf8ea297ab18a32d1ba
Randall Adams <rjadams96@gmail.com>
2021-12-31 20:08:49 -0500

    update assesment script

--- sql_for_assessment.sql
+++ sql_for_assessment.sql
@@ -1,16 +1,16 @@
--- First create the customer_film_profile table
+-- STEP 1 create the customer_film_profile table
 
 CREATE TABLE public.customer_film_profile
 (
-    customer_film_profile_id integer NOT NULL DEFAULT nextval('customer_film_profile_customer_film_profile_id_seq'::regclass),
+    customer_file_profile_id integer NOT NULL DEFAULT nextval('customer_film_profile_customer_file_profile_id_seq'::regclass),
     customer_id integer NOT NULL,
     all_rentals_description text COLLATE pg_catalog."default" NOT NULL,
     all_rentals_fulltext tsvector NOT NULL,
+    CONSTRAINT customer_film_profile_pkey PRIMARY KEY (customer_file_profile_id),
     CONSTRAINT customer_film_profile_customer_id FOREIGN KEY (customer_id)
         REFERENCES public.customer (customer_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-        NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -18,20 +18,9 @@ TABLESPACE pg_default;
 ALTER TABLE public.customer_film_profile
     OWNER to postgres;
 
+-- End of STEP 1
 
-CREATE INDEX all_rentals_fulltext_idx
-    ON public.customer_film_profile USING gist
-    (all_rentals_fulltext)
-    TABLESPACE pg_default;
-
-CREATE INDEX fki_customer_film_profile_customer_id
-    ON public.customer_film_profile USING btree
-    (customer_id ASC NULLS LAST)
-    TABLESPACE pg_default;
-
--- End of create customer_file_profile
-
--- Populate our customer_film_profile
+-- STEP 2 populate our customer_film_profile
 INSERT INTO public.customer_film_profile
     SELECT
         nextval('customer_film_profile_customer_film_profile_id_seq'),
@@ -45,8 +34,9 @@ INSERT INTO public.customer_film_profile
             JOIN film f ON f.film_id = i.film_id
 GROUP BY c.customer_id;
 
--- End of profile population
+-- End of STEP 2
 
+ -- Option update script
 UPDATE public.customer_film_profile SET
     all_rentals_description = (
         SELECT string_agg(f.description, ' ') as agg_description
@@ -76,7 +66,7 @@ WHERE customer_id = new.customer_id;
             JOIN film f ON f.film_id = i.film_id
 WHERE c.customer_id 1;
 
--- Generate report of recommended movies
+-- STEP 3 Generate report of recommended movies
 
 SELECT
 	f.film_id,
@@ -101,9 +91,10 @@ WHERE film_id NOT IN (SELECT f2.film_id
  		WHERE c.customer_id = cfp.customer_id)
 ORDER BY similarity_score DESC LIMIT 5;
 
--- End generate report of recommended movies
+-- End of STEP 3
 
--- REport of mv for next recommended movie:
+-- STEP 4 Report of mv for next recommended movie:
+
 CREATE MATERIALIZED VIEW public.next_recommended_film
 TABLESPACE pg_default
 AS
@@ -113,14 +104,15 @@ AS
           ORDER BY (ts_rank(film.fulltext, plainto_tsquery(cfp.all_rentals_description))) DESC
          LIMIT 1) AS film_id
    FROM customer_film_profile cfp
+   WHERE cfp.customer_id BETWEEN 1 and 10
 WITH DATA;
 
 ALTER TABLE public.next_recommended_film
     OWNER TO postgres;
 
--- end sql for mv of next recommended movie
+-- END of STEP 4 
 
--- Create trigger function:
+-- STEP 5 Create trigger function:
 CREATE FUNCTION public.update_customer_profile()
     RETURNS trigger
     LANGUAGE 'plpgsql'
@@ -153,8 +145,9 @@ $BODY$;
 ALTER FUNCTION public.update_customer_profile()
     OWNER TO postgres;
 
--- End creation of trigger function
--- Create trigger on rental table
+-- End of STEP 5 of trigger function
+
+-- STEP 6 Create trigger on rental table
 
 CREATE TRIGGER update_customer_profile
     AFTER INSERT
@@ -162,9 +155,9 @@ CREATE TRIGGER update_customer_profile
     FOR EACH ROW
     EXECUTE FUNCTION public.update_customer_profile();
 
--- End create trigger
+-- End STEP 6
 
--- Insert to verify trigger
+-- STEP 7 Insert to verify trigger
 
 EXPLAIN ANALYZE INSERT INTO public.rental(
 	rental_id, rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
@@ -178,15 +171,14 @@ EXPLAIN ANALYZE INSERT INTO public.rental(
 		current_timestamp
 	);
 
--- End verification of trigger
+-- End of STEP 7
 
--- Stored procedure of recommendations
+-- STEP 8 Stored procedure of recommendations
 -- PROCEDURE: public.refresh_customer_profile()
 
 -- DROP PROCEDURE public.refresh_customer_profile();
 
-CREATE OR REPLACE PROCEDURE public.refresh_customer_profile(
-	)
+CREATE OR REPLACE PROCEDURE public.refresh_customer_profile()
 LANGUAGE 'plpgsql'
 AS $BODY$
 BEGIN
@@ -207,3 +199,5 @@ COMMIT;
 REFRESH MATERIALIZED VIEW next_recommended_film;
 END;
 $BODY$;
+
+-- END OF DEMO
